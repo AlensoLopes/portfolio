75c6b4720c841e617c3fb003d40c3634e182ee01
Alenso Lopes <alenso.lopes@gmail.com>
2023-09-29 09:47:57 +0200

    💄style: Header link to project page & footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,10 +5,11 @@ export const Footer = () => {
   return (
     <footer className='border-t-2 border-secondary-450 flex flex-col
       justify-center items-center bg-secondary-500 py-4 px-4'>
-      <h1 className='text-white font-bold text-center text-2xl py-1'>Alenso.L</h1>
+      <h1 className='text-white font-bold text-center text-2xl py-1'>
+        Alenso.L</h1>
       <div>
         <p className='text-white opacity-80 font-medium'>
-          &copy; Alenso {date}. All right reserved.
+        &copy; {date}. All right reserved.
         </p>
       </div>
     </footer>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,11 +4,18 @@ const Picture = 'https://avatars.githubusercontent.com/u/46823570?v=4';
 
 export const Header = () => {
   return (
-    <header className='px-6 py-4 flex flex-row
-      justify-between items-center sticky top-0 bg-secondary-500'>
-      <h1 className='text-2xl text-white'>Alenso.L</h1>
-      <div className='flex flex-row items-center'>
+    <header className='px-5 lg:px-36 py-4 flex flex-row
+      justify-between items-center sticky top-0 bg-secondary-500 backdrop-blur-sm'>
+      <div className='flex flex-row items-center lg:px-10 gap-4 px-4'>
         <img src={Picture} alt='' className='w-10 h-10 rounded-full'/>
+        <h1 className='text-2xl text-white'>AL.</h1>
+      </div>
+      <div className='flex flex-row items-center'>
+        <h1 className='text-white text-xl'>
+          <a href=''>
+            Projects
+          </a>
+        </h1>
       </div>
     </header>
   );
