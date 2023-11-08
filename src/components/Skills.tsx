087fda2e82e8dd0cb553a0b2799f76bc37f4475d
Alenso Lopes <alenso.lopes@gmail.com>
2023-11-08 21:51:59 +0100

    feat: api call to get logo

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,18 +1,35 @@
+import { get } from 'http';
 import * as React from 'react';
-import { API_LOGO_KEY }  from '../assets/conf/config.json';
-const API_URL = 'https://api.api-ninjas.com/v1/logo?name=';
+// import { API_LOGO_KEY }  from '../assets/conf/config.json';
+
+const api_key = 'YmWmVjnrHAnGAj/kjd/qts3pEJZKfktVI8+ANI/yQfE=';
+const API_URL = 'https://api.brandfetch.io/v2/brands/';
 
 async function getLogo(companyName : string){
   const url = API_URL + companyName;
   const response = await fetch(url, {
     method: 'GET',
     headers: {
-      'X-Api-Key': '1d4f3e0b-7a0e-4a6e-8a3e-4e5b2d8a2a5a'
+      'Authorization': 'Bearer ' + api_key
     }
-  });
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
+    })
+    .then((data) => {
+      return data;
+    })
+    .catch((error) => {
+      console.error('Fetch error:', error);
+    });
+  return response.logos[0].formats[0].src;
 }
 
 export const Skills = () => {
+  const react = getLogo('Microsoft.com');
   return (
     <div className='flex justify-center bg-secondary-480 flex-col border-t
     border-secondary-450 py-2 items-center'>
@@ -23,7 +40,6 @@ export const Skills = () => {
         <h1 className='text-4xl text-white gap-4'>FrontEnd</h1>
         <h2 className='text-2xl text-white'>Technologies</h2>
         <div className='flex flex-wrap'>
-          test
         </div>
       </div>
     </div>
