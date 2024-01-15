51151922c4e51571308f48d279b64dc1557434cb
Alenso Lopes <alenso.lopes@gmail.com>
2024-01-15 16:13:35 +0100

    feat: favicon and project description

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Router } from './router/Router';
 
 function App() {
+  useEffect(() => {
+    document.title = 'Alenso Lopes | Portfolio';
+  }, []);
   return (
     <Router />
   );
